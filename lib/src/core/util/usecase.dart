c8048461a1c3baa5dee19fc6f6ee890d03472780
Gwanhyeong Lee <wudy343@gmail.com>
2021-11-19 16:25:57 +0900

    Update usecase.dart

--- lib/src/core/util/usecase.dart
+++ lib/src/core/util/usecase.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class UseCase<DataType, Params> {
-  Future<Either<Exception, DataType>> call(Params params);
+  Future<DataType> call(Params params);
 }
 
 class NoParams extends Equatable {
